11a9c64130cf4c54091270968fbf15f960742ba2
jajupoik <janne.poikolainen@jyu.fi>
2017-05-02 16:26:00 +0200

    Update mqtt_temp_sensor_node.cpp

--- mqtt_temp_sensor_node/mqtt_temp_sensor_node.cpp
+++ mqtt_temp_sensor_node/mqtt_temp_sensor_node.cpp
@@ -18,8 +18,8 @@ long lastMsg = 0;
 long crrMsgNo = 0;
 
 // INSERT WIFI CREDENTIALS AND MQTT-SERVER ADDRESS
-const char* ssid     	= "AgoraOpen"; 	// wifi ssid;
-const char* password 	= ""; 			// wifi password;
+const char* ssid     	= ""; 	// wifi ssid;
+const char* password 	= ""; 		// wifi password;
 const char* mqtt_server = "127.0.0.1";	// MQTT broker address
 const char* topic_temp 	= "Temperature";
 
